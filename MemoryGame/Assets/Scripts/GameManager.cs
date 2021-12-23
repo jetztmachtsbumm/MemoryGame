65dcccba3b97cb88bcacf04c1a442be154954d89
jetztmachtsbumm <68288315+jetztmachtsbumm@users.noreply.github.com>
2021-12-23 11:27:23 +0100

    Card mix algorithm

--- MemoryGame/Assets/Scripts/GameManager.cs
+++ MemoryGame/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class GameManager : MonoBehaviour
 {
@@ -8,14 +9,35 @@ public class GameManager : MonoBehaviour
     [SerializeField] private AudioClip clipCardUp;
     [SerializeField] private AudioClip clipCardDown;
     [SerializeField] private AudioClip clipCardMatch;
+    [SerializeField] private GameObject[] allCards;
 
     private MemoryCard firstSelectedCard;
     private MemoryCard secondSelectedCard;
+    private List<Vector3> allCardPositions = new List<Vector3>();
     private bool canClick = true;
+
+    private void Awake()
+    {
+        //Get all card positions
+        foreach(GameObject card in allCards)
+        {
+            allCardPositions.Add(card.transform.position);
+        }
+
+        //Randomize card positions
+        System.Random ran = new System.Random();
+        allCardPositions = allCardPositions.OrderBy(position => ran.Next()).ToList();
+
+        //Assign new positions
+        for (int i = 0; i < allCards.Length; i++)
+        {
+            allCards[i].transform.position = allCardPositions[i];
+        }
+    }
 
     public void OnCardClicked(MemoryCard card)
     {
-        if (!canClick) return;
+        if (!canClick || card == firstSelectedCard) return;
 
         card.SetTargetRotation(90);
         card.SetTargetHeight(0.05f);
